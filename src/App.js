31d9b1065ed925d50c5ce24739dbf8f539687c6c
Jamaloooo <jamalsa3d2001@gmail.com>
2023-03-03 07:59:04 +0200

    9- Optimistic vs Pessimistic Updates

--- src/App.js
+++ src/App.js
@@ -33,10 +33,18 @@ class App extends Component {
   };
 
   handleDelete = async (post) => {
-    await axios.delete(apiEndpoint + "/" + post.id);
+    const originalPosts = this.state.posts;
 
     const posts = this.state.posts.filter((p) => p.id !== post.id);
     this.setState({ posts });
+
+    try {
+      await axios.delete(apiEndpoint + "/" + post.id);
+      throw new Error("");
+    } catch (ex) {
+      alert("Something failed while deleting post!");
+      this.setState({ posts: originalPosts });
+    }
   };
 
   render() {
